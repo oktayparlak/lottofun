a88bf9bbc8792005606d00f46525b36f93d38a57
Oktay Parlak <oktay.parlak.12@hotmail.com>
2025-05-20 23:20:27 +0300

    initial commit

---
+++ src/main/java/com/oktayparlak/lottofun/LottofunApplication.java
@@ -0,0 +1,13 @@
+package com.oktayparlak.lottofun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottofunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottofunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oktayparlak/lottofun/LottofunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oktayparlak.lottofun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottofunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
